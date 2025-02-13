22f4cb2fa48e2f7691c97d7bfe6ce2d2c70b24e9
Lev <emai@.com>
2025-02-13 20:45:50 +0300

    feat: Структурированый вывод

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 import os
 import json
 import re
+import nltk
+
+nltk.download('punkt_tab')
 
 def read_json(file_name):
     if not os.path.exists(file_name):
@@ -19,27 +22,36 @@ def read_json(file_name):
             sys.exit(1)
         return f_json['url'], f_json['output']
 
-def save_json(file_name, data):
-    pass
+def save_json(file_name, sentences):
+    with open(file_name, 'w', encoding='utf-8') as f:
+        json.dump(sentences, f, ensure_ascii=False, indent=4)
+
+def preprocess_text(text):
+    text = re.sub(r'\r\n', '. ', text)
+    text = re.sub(r'\t', '', text)
+    text = re.sub(r'\.\.', r'.', text)
+    return text
 
 def main():
     try:
+
         settings_file = 'settings.json'
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
         }
-        url, out_file = read_json(settings_file)
 
-        request = requests.get(url, headers=headers)
-
-        if request.status_code == 200:
-            soup = BeautifulSoup(request.content, 'html.parser')
-            text = soup.find(name='div', class_="rasp_txt").text
-            print(text)
-        else:
-            print(f"Ошибка при запросе {request.status_code}")
-            sys.exit(1)
-
+        urls, out_files = read_json(settings_file)
+        for url, out_file in zip(urls, out_files):
+            request = requests.get(url, headers=headers)
+            if request.status_code == 200:
+                soup = BeautifulSoup(request.content, 'html.parser')
+                text = soup.find(name='div', class_="rasp_txt").text
+                text = preprocess_text(text)
+                text = nltk.sent_tokenize(text, language='russian')
+                save_json(out_file, text)
+            else:
+                print(f"Ошибка при запросе {request.status_code}")
+                sys.exit(1)
     except FileNotFoundError as e:
         print(e)
     except ValueError as e:
