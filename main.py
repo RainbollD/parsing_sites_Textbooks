55455f4781cc7e25af0e6c144351a79d7839bf13
Lev <emai@.com>
2025-02-24 09:17:03 +0300

    clean: comment functions

--- main.py
+++ main.py
@@ -14,14 +14,20 @@ class GetSettings:
 
     @staticmethod
     def is_file():
+        """Проверка на наличие файла"""
         if not os.path.exists(FILE_SETTINGS):
             raise FileNotFoundError(f"Файл {FILE_SETTINGS} не найден.")
 
     def is_dict(self):
+        """Проверка, что передаваемы данные - это словарь"""
         if not isinstance(self.file_data, dict):
             raise ValueError("Неверный формат данных в файле.")
 
     def read_json(self):
+        """
+        Чтение из файла settings.json
+        Загрузка данных книг в self.file_data
+        """
         self.is_file()
         with open(FILE_SETTINGS, 'r', encoding="utf8") as data:
             self.file_data = json.load(data)
@@ -32,6 +38,11 @@ class GetSettings:
 class TransformData:
 
     def del_begin(self):
+        """
+        Поиск момента с которого начинаются упражнения,
+        путем поиска наличия особого символа в строке "KEY_SYMBOLS_BEG"
+        :return:
+        """
         for idx, line in enumerate(self.text):
             for key in KEY_SYMBOLS_BEG:
                 if key in line:
@@ -39,6 +50,13 @@ class TransformData:
                     return
 
     def del_end(self):
+        """
+        Поиск момента с которого упражнения заканчиваются,
+        путем поиска наличия особого символа в строке "KEY_SYMBOLS_BEG"
+        и сохранения индекса этой строки.
+        Выход из функции, когда будет просмотрено 20% текста
+        :return:
+        """
         idx_del = 0
         for idx, line in enumerate(self.text[::-1]):
             if idx > len(self.text) * 0.2:
@@ -49,10 +67,19 @@ class TransformData:
                     idx_del = idx
 
     def find_exercise(self, is_del_end):
+        """
+        Организация поИсика упражнений.
+        :param is_del_end: Нужно ли удалять конец
+        :return:
+        """
         self.del_begin()
         if is_del_end: self.del_end()
 
     def transform_text(self):
+        """
+        Преобразование сырых данных из pdf
+        :return:
+        """
         self.text = re.sub(r'begin.*?\n', ' ', self.text)
         self.text = re.sub(r'.*?end', '', self.text)
         self.text = re.sub(r'\(cid:\d{1,3}\)\n', '', self.text)
@@ -65,6 +92,12 @@ class TransformData:
         self.text = nltk.sent_tokenize(self.text, language='russian')
 
     def control_transform(self, is_del_end, find_exersice):
+        """
+        Вызов трансформации текста и поиска упражнений
+        :param is_del_end: Нужно ли удалять конец
+        :param find_exersice: Нужно ли искать упражнения
+        :return:
+        """
         self.transform_text()
         if find_exersice: self.find_exercise(is_del_end)
 
@@ -80,11 +113,22 @@ class BasicControl(GetSettings, TransformData):
         self.up_down = 'up'
 
     def is_request(self, request):
+        """
+        Проверка запроса
+        :param request:
+        :return:
+        """
         if not request.status_code == 200:
             raise requests.ConnectionError("Status code {}, url: {}".format(request.status_code, self.url))
 
     @staticmethod
     def is_special_page(page):
+        """
+        Проверка на особую страницу (страницы не содержащей упражнения),
+        путем поиска в начале страницы особых символов "KEY_SYMBOLS_END"
+        :param page: Страница, полученная из pdf
+        :return:
+        """
         for key in KEY_SYMBOLS_END:
             for line in page.split('\n')[:2]:
                 if line == key or key.upper() == line:
@@ -92,27 +136,53 @@ class BasicControl(GetSettings, TransformData):
         return False
 
     def is_url_or_file(self):
+        """
+        Проверка, self.url - ссылка на сайт или файл pdf
+        :return:
+        """
         regex = re.compile(r'^(?:http|ftp)s?://', re.IGNORECASE)
         return re.match(regex, self.url) is not None
 
     def assign_value_to_variable(self, book):
+        """
+        Присваивание: self.title - название книги,
+                      self.url - ссылка на сайт или файл
+                      self.remove_pages - странцы, которые нужно удалить
+        :param book: Данные книги
+        :return:
+        """
         self.title = book['title']
         self.url = book['url']
         self.remove_pages = book['remove_pages']
 
     def download_file_from_url(self):
+        """
+        Скачивание файла и сохранение под именем self.name_file
+        :return:
+        """
         response = requests.get(self.url)
         self.is_request(response)
         with open(self.name_file, 'wb') as f:
             f.write(response.content)
 
     def add_begin_end(self, page):
+        """
+        Добавление специальных обозначений в начало или конец страницы,
+        для определения в дальнейшем места оглавления
+        :param page: Страница книги
+        :return:
+        """
         if self.up_down == 'up':
             self.text += 'begin' + page
         else:
             self.text += page + 'end'
 
     def remove_pages_from_settings(self, pdf):
+        """
+        Удаление страниц из массива self.remove_pages
+        :param pdf: Pdf книга
+        :return:
+        """
         self.text = ''
         idx = 0
         for page in pdf:
@@ -122,6 +192,13 @@ class BasicControl(GetSettings, TransformData):
         return True
 
     def remove_special(self, pdf):
+        """
+        Перебирает страницы книги для выявления особых страниц (не содержащих страниц).
+        Не сохраняет данные особой страницы.
+        :param pdf: Pdf книга
+        :return: True: Не найдено осоых страниц после 75%
+                 False: После 75% текста, если находит особую странцу
+        """
         self.text = ''
         idx = 0
         len_pdf_pages = len(pdf)
@@ -135,6 +212,11 @@ class BasicControl(GetSettings, TransformData):
         return True
 
     def up_down_contents(self, pdf):
+        """
+        Определение расположения нумерации
+        :param pdf: Pdf книга
+        :return:
+        """
         self.up_down = 'up'
         for page in pdf[10:20]:
             string = page.extract_text().split('\n')[0]
@@ -143,6 +225,13 @@ class BasicControl(GetSettings, TransformData):
                 return
 
     def extract_text_with_pdfplumber(self):
+        """
+        Открытие файла
+        Получение расположение нумерации
+        Удаление особых страниц (не содержащих упражений)
+        :return: Нужно ли удалять конец True: Да
+                                        False: Нет
+        """
         with pdfplumber.open(self.name_file) as pdf:
             pdf = pdf.pages
             self.up_down_contents(pdf)
@@ -151,10 +240,20 @@ class BasicControl(GetSettings, TransformData):
             return self.remove_special(pdf)
 
     def save_json(self):
-        with open(self.title, 'w', encoding='utf-8') as f:
+        """
+        Сохранение книги в test_files
+        :return:
+        """
+        with open('test_file/' + self.title, 'w', encoding='utf-8') as f:
             json.dump(self.text, f, ensure_ascii=False, indent=4)
 
     def download(self, book):
+        """
+        Присваивает данные
+        и скачивает файл, если дана ссылка
+        :param book:
+        :return:
+        """
         self.assign_value_to_variable(book)
         if self.is_url_or_file():
             self.name_file = DEFAULT_NAME_PDF
@@ -163,6 +262,10 @@ class BasicControl(GetSettings, TransformData):
             self.name_file = self.url
 
     def main(self):
+        """
+        Метод, контролирующий классы.
+        :return:
+        """
         self.read_json()
         for book in self.file_data:
             self.download(book)
